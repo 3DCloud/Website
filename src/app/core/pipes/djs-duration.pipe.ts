4de928d06af53e795ce1f2b3500d9760ccff9187
nicoco007 <nicolas@gnyra.com>
2021-11-05 16:50:59 -0400

    Show 'Less that 1 minute' instead of 0 minutes

--- src/app/core/pipes/djs-duration.pipe.ts
+++ src/app/core/pipes/djs-duration.pipe.ts
@@ -8,9 +8,11 @@ export class DjsDurationPipe implements PipeTransform {
   transform(value: number, ...args: unknown[]): string {
     if (args[0]) {
       if (value > 1_000 * 60 * 60) {
-        return dayjs.duration(value).format('H[h] m[m]');
+        return dayjs.duration(value).format('H [hours], m [minutes]');
+      } else if (value >= 1_000 * 60) {
+        return dayjs.duration(value).format('m [minutes]');
       } else {
-        return dayjs.duration(value).format('m[m]');
+        return 'Less than 1 minute';
       }
     } else {
       if (value > 1_000 * 60 * 60) {
